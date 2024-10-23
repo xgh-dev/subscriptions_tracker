c160457681f3603dff512e6f16023f3fdb643d9b
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2024-10-23 14:37:42 -0600

    componetes anexados 1.0

---
+++ Subscription_tracker/src/components/FormAddMoney.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const FormAddMoney = ({ setCount, setIsValid }) => {
+    const [input, setInput] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleForm = e => {
+        e.preventDefault();
+        if (input === "" || Number(input) < 0) {
+            setError(true);
+            return;
+        }
+        setError(false);
+        setCount(Number(input));
+        setIsValid(true);
+        //console.log(input);
+    }
+
+    return ( 
+        <div className="form-add-money">
+            <form onSubmit={ handleForm }>
+                <p>Agregar Presupuesto</p>
+                <input type="number" placeholder="$$$" onChange={e => setInput(e.target.value)} />
+                <input type="submit" value="Agregar" />
+            </form>
+            { error ? <p className="error">Presupuesto invalido</p> : null }
+            
+        </div>
+    );
+}
+ 
+export default FormAddMoney;
---
+++ Subscription_tracker/src/components/Header.jsx
@@ -0,0 +1,11 @@
+//definimos la funcion que sera el componente header
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Subscriptions Tracker</h2>
+        </div>
+    );
+}
+
+export default Header
